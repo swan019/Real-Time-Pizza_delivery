238a4d4764b2164922d9100e9bd61bc4d2a9de26
swan019 <swapnilingale019@gmail.com>
2024-05-27 12:14:33 +0530

    Setup Completed

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
 
+//assets
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
     res.render('home');
 })
+
+
 
 // set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
 
 
 app.listen(PORT, () => {
