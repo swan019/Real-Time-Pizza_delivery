514013bdb8b0b0747ffba72e83c3e55ac342f4ff
swan019 <swapnilingale019@gmail.com>
2024-05-27 17:24:24 +0530

    add cart, login , registration pages

--- server.js
+++ server.js
@@ -9,19 +9,24 @@ const PORT = process.env.PORT || 3000
 //assets
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-
-
 // set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+})
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+})
 
 app.listen(PORT, () => {
     console.log(`Listen at ${PORT}`);
